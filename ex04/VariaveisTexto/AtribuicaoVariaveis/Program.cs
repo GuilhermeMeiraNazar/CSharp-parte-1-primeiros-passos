41648660fbec06193d75b8cb755beefddb2579b2
Guilherme De Meira Nazar <guilhermen610@gmail.com>
2025-05-07 08:01:27 -0300

    String

---
+++ ex04/VariaveisTexto/AtribuicaoVariaveis/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtribuicaoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ex04");
+
+            int idade = 32;
+            int idadeGui = idade;
+
+            Console.WriteLine(idadeGui);
+
+            Console.WriteLine("Execução acabou ...");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ex04/VariaveisTexto/VariaveisTexto/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nome
+{
+    class VariaveisTexto
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aula 4");
+
+            char primeiraLetra = '5';
+            primeiraLetra = (char)65;
+            //para representar 1 caracter tenho que usar aspas simples
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Ai que gostoso" + ' ' +2025;
+            Console.WriteLine(titulo);
+
+            string lista = 
+                "item 1 " +
+                "Item 2" +
+                "Item 3" +
+                "Item 5";
+            Console.WriteLine(lista);
+
+            Console.WriteLine("Execução acabou ...");
+            Console.ReadLine();
+
+        }
+    }
+}
