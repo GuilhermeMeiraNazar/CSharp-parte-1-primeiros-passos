79171a506088894923b2e4b1237f668ff72fb82b
Guilherme De Meira Nazar <guilhermen610@gmail.com>
2025-05-07 14:20:23 -0300

    Add files via upload

---
+++ ex06/Laço_Repetição/Break/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 6");
+
+            for(int contadorLinha =0; contadorLinha<10; contadorLinha++)
+            {
+                for(int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if(contadorColuna >= contadorLinha)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine("Execução acabou ...");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ex06/Laço_Repetição/For/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 6");
+
+            double valorInvestido = 1000;
+
+            for(int contadorMes = 1; contadorMes <= 12; contadorMes = contadorMes + 1)
+            // for = se for verdadeiro então ....
+            {
+                valorInvestido *= 1.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, Você terá R$" + valorInvestido);
+            }
+
+
+            Console.WriteLine("Execução acabou ...");
+            Console.ReadLine();
+
+        }
+    }
+}
